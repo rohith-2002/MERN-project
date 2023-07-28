de1ac6051ffec86016d5a1680b94b7b9a0c29f22
rohith <rohithgundu11949@gmail.com>
2023-07-29 02:53:58 +0530

    change get request

--- client/src/scenes/widgets/UserWidget.jsx
+++ client/src/scenes/widgets/UserWidget.jsx
@@ -22,7 +22,7 @@ const UserWidget = ({ userId, picturePath }) => {
     const main = palette.neutral.main;
 
     const getUser = async () => {
-        const response = await fetch(`http://localhost:3001/users/${userId}`, {
+        const response = await fetch(`https://socialmedia-3i6a.onrender.com/users/${userId}`, {
             method: "GET",
             headers: { Authorization: `Bearer ${token}` },
         });
